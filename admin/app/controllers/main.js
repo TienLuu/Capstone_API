bb285c9d08a7e68f10b3ffe61c76cdfe85cc7c4c
TienLuu <tienluu1301@gmail.com>
2022-09-22 13:43:30 +0700

    Fixes error toast admin page

--- admin/app/controllers/main.js
+++ admin/app/controllers/main.js
@@ -1,26 +1,46 @@
-import ProductManager from "../model/ProductManager.js"
-import Product, { addProduct, deleteProduct, getProductByID, getProductByValue, getProducts, updateProduct } from "../model/Product.js";
-import validationForm, { resetError } from "../services/validate.js"
-import { errorToast, infoToast, successToast } from "../services/toastMessage.js"
-
-
+import ProductManager from "../model/ProductManager.js";
+import Product, {
+   addProduct,
+   deleteProduct,
+   getProductByID,
+   getProductByValue,
+   getProducts,
+   updateProduct,
+} from "../model/Product.js";
+import validationForm, { resetError } from "../services/validate.js";
+import {
+   errorToast,
+   infoToast,
+   successToast,
+} from "../services/toastMessage.js";
 
 //=====================================================
 //       Tạo hàm render các sản phẩm ra giao diện
 //         từ thông tin nhận được khi gọi API
 //=====================================================
 function initial() {
-    getProducts()
-        .then(products => {
-            productManager.products = products
-            render(productManager.products)
-        })
+   getProducts().then((products) => {
+      productManager.products = products;
+      render(productManager.products);
+   });
 }
 // hàm render phần giao diện bảng để quản lý
 function render(products) {
-    let htmls = products.reduce((output, product, index) => {
-        const { id, name, price, screen, backCamera, frontCamera, img, desc, brand } = product
-        return output + `
+   let htmls = products.reduce((output, product, index) => {
+      const {
+         id,
+         name,
+         price,
+         screen,
+         backCamera,
+         frontCamera,
+         img,
+         desc,
+         brand,
+      } = product;
+      return (
+         output +
+         `
             <div class="product__item" data-toggle="modal" data-target="#previewModal" data-id="${id}">
                 <div class="row">
                     <div class="product__id col-2 col-sm-1 col-md-1">
@@ -50,16 +70,30 @@ function render(products) {
                     </div>
                 </div>
             </div>`
-    }, '')
-    if (!htmls) htmls = `<div class="product__empty">No product found</div>`
-    productManager.products = products
-    dom("#product__content").innerHTML = htmls
+      );
+   }, "");
+   if (!htmls) htmls = `<div class="product__empty">No product found</div>`;
+   productManager.products = products;
+   dom("#product__content").innerHTML = htmls;
 }
 
 // hàm render phần preview , chứa tất cả thông tin sản phẩm
 function renderPreview(product) {
-    const { id, productCode, name, price, screen, backCamera, frontCamera, img, desc, brand, views, solds } = product
-    const htmls = `
+   const {
+      id,
+      productCode,
+      name,
+      price,
+      screen,
+      backCamera,
+      frontCamera,
+      img,
+      desc,
+      brand,
+      views,
+      solds,
+   } = product;
+   const htmls = `
         <div class="overlay">
         </div>
         <div class="preview">
@@ -118,207 +152,190 @@ function renderPreview(product) {
                 <i class='bx bx-x'></i>
             </div>
         </div>
-        `
-    dom("#previewModal").innerHTML = htmls
+        `;
+   dom("#previewModal").innerHTML = htmls;
 }
 
 // hàm reRender để reRender lại mỗi khi cập nhật,chỉnh sửa thông tin
 function reRender() {
-    initial()
+   initial();
 }
-
-
-
 
 //=====================================================
 //       Xử lý tạo giao diện đầu tiên khi vừa vào web
 //=====================================================
 let productManager = new ProductManager();
-initial()
-
-
-
-
+initial();
 
 //=====================================================
-//          Lắng nghe sự kiện click vào 
+//          Lắng nghe sự kiện click vào
 //     các button chức năng xử lý thêm sửa xóa update
 //=====================================================
 document.addEventListener("click", (e) => {
-    e.preventDefault()
-    const button = e.target
+   e.preventDefault();
+   const button = e.target;
 
-    // Mặc định các button có chức năng xử lý form đều có property [data-type]
-    // Kiểm tra nếu nhấn phải các element không có data-type thì không xử lý gì cả
-    const buttonType = button.dataset.type
-    if (!buttonType) return
-    button.disabled = true
+   // Mặc định các button có chức năng xử lý form đều có property [data-type]
+   // Kiểm tra nếu nhấn phải các element không có data-type thì không xử lý gì cả
+   const buttonType = button.dataset.type;
+   if (!buttonType) return;
+   button.disabled = true;
 
+   // Xử lý khi nhấn nút mở form nhập sản phẩm
+   if (buttonType === "open") {
+      resetFormValue();
+      resetError();
+      dom("#productCode").disabled = false;
 
-    // Xử lý khi nhấn nút mở form nhập sản phẩm
-    if (buttonType === "open") {
-        resetFormValue()
-        resetError()
-        dom("#productCode").disabled = false
-
-        dom("#formModal .form__header").innerText = "Add New Product"
-        dom("#formModal .form__submit").innerHTML =
-            `<button class="submitBtn" data-type="add">Confirm</button>
+      dom("#formModal .form__header").innerText = "Add New Product";
+      dom(
+         "#formModal .form__submit"
+      ).innerHTML = `<button class="submitBtn" data-type="add">Confirm</button>
              <button class="cancleBtn" data-toggle="modal" data-target="#formModal">Cancel</button>
-        `
-        button.disabled = false
+        `;
+      button.disabled = false;
+   }
 
-    }
+   // Xử lý khi nhấn nút xác nhận (confirm) tạo mới sản phẩm
+   if (buttonType === "add") {
+      const product = getFormValue();
 
+      validationForm()
+         .then(() => {
+            return new Promise((resolve, reject) => {
+               // Kiểm tra xem productcode có trùng trong database không
+               // nếu trùng thì reject
+               // không trùng thì tiếp tục resolve
+               getProductByValue({ productCode: product.productCode })
+                  .then(() => {
+                     reject("same product code");
+                  })
+                  .catch(() => {
+                     resolve();
+                  });
+            });
+         })
 
-    // Xử lý khi nhấn nút xác nhận (confirm) tạo mới sản phẩm
-    if (buttonType === "add") {
-        const product = getFormValue()
+         .then(() => {
+            return addProduct(product);
+         })
+         .then((product) => {
+            productManager.add(product);
+            closeForm();
+            reRender();
+            successToast.fire(`"${product.name}" is ready to sell 🤞🎁`);
+         })
+         .catch((error) => {
+            if (error === "same product code") {
+               errorToast.fire("💢 productCode's name has already been used");
+            } else if (error === false) {
+               errorToast.fire("Your form input value is not valid 🏴 🏴 🏴");
+            } else {
+               errorToast.fire("Sever is busy 🏴 🏴 🏴");
+            }
+         })
+         .finally(() => {
+            button.disabled = false;
+         });
+   }
 
-        validationForm()
-            .then(() => {
-                return new Promise((resolve, reject) => {
-                    // Kiểm tra xem productcode có trùng trong database không
-                    // nếu trùng thì reject
-                    // không trùng thì tiếp tục resolve
-                    getProductByValue({ "productCode": product.productCode })
-                        .then(() => {
-                            reject("same product code")
-                        })
-                        .catch(() => {
-                            resolve()
-                        })
-                })
-            })
+   // Xử lý khi nhấn nút delete sản phẩm
+   if (buttonType === "delete") {
+      const produceID = button.closest(".product__item").dataset.id;
 
-            .then(() => {
-                return addProduct(product)
-            })
-            .then(product => {
-                productManager.add(product)
-                closeForm()
-                reRender()
-                successToast.fire(`"${product.name}" is ready to sell 🤞🎁`)
-            })
-            .catch((error) => {
-                if (error = "same product code") {
-                    errorToast.fire("💢 productCode's name has already been used")
-                }
-                else {
-                    errorToast.fire("Sever is busy 🏴 🏴 🏴")
-                }
-            })
-            .finally(() => {
-                button.disabled = false
-            })
-    }
+      deleteProduct(produceID)
+         .then((product) => {
+            productManager.delete(product.id);
+            reRender();
+            infoToast.fire(`You have deleted ಠ_ಠ "${product.name}"`);
+         })
+         .catch((error) => {
+            errorToast.fire("Sever is busy 🏴 🏴 🏴");
+         })
+         .finally(() => {
+            button.disabled = false;
+         });
+   }
 
+   // Xử lý khi nhấn nút edit sản phẩm
+   if (buttonType === "edit") {
+      resetFormValue();
+      resetError();
+      // Không cần sửa lại productCode
+      dom("#productCode").disabled = true;
 
-    // Xử lý khi nhấn nút delete sản phẩm
-    if (buttonType === "delete") {
-        const produceID = button.closest(".product__item").dataset.id
+      const produceID = button.closest(".product__item").dataset.id;
 
-        deleteProduct(produceID)
-            .then(product => {
-                productManager.delete(product.id)
-                reRender()
-                infoToast.fire(`You have deleted ಠ_ಠ "${product.name}"`)
-            })
-            .catch((error) => {
-                errorToast.fire("Sever is busy 🏴 🏴 🏴")
-            })
-            .finally(() => {
-                button.disabled = false
-            })
-    }
-
-
-    // Xử lý khi nhấn nút edit sản phẩm
-    if (buttonType === "edit") {
-        resetFormValue()
-        resetError()
-        // Không cần sửa lại productCode
-        dom("#productCode").disabled = true
-
-        const produceID = button.closest(".product__item").dataset.id
-
-        dom("#formModal .form__header").innerText = "Edit Product"
-        dom("#formModal .form__submit").innerHTML =
-            `<button class="submitBtn" data-type="update">Update</button>
+      dom("#formModal .form__header").innerText = "Edit Product";
+      dom(
+         "#formModal .form__submit"
+      ).innerHTML = `<button class="submitBtn" data-type="update">Update</button>
              <button class="cancleBtn" data-toggle="modal" data-target="#formModal">Cancel</button>
-        `
+        `;
 
-        getProductByID(produceID)
-            .then(product => {
-                fillFormValue(product)
-            })
-            .catch((error) => {
-                console.log(error)
-            })
-            .finally(() => {
-                button.disabled = false
-            })
-    }
+      getProductByID(produceID)
+         .then((product) => {
+            fillFormValue(product);
+         })
+         .catch((error) => {
+            console.log(error);
+         })
+         .finally(() => {
+            button.disabled = false;
+         });
+   }
 
+   // Xử lý khi nhấn nút update sản phẩm
+   if (buttonType === "update") {
+      const product = getFormValue();
 
-    // Xử lý khi nhấn nút update sản phẩm
-    if (buttonType === "update") {
-        const product = getFormValue()
-
-        validationForm()
-            .then(() => {
-                return updateProduct(product, product.id)
-            })
-            .then(product => {
-                productManager.update(product.id, product)
-                closeForm()
-                reRender()
-                successToast.fire(`Update ${product.name} successfully 🎉`)
-            })
-            .catch((error) => {
-                errorToast.fire("Sever is busy 🏴 🏴 🏴")
-            })
-            .finally(() => {
-                button.disabled = false
-            })
-    }
-})
-
+      validationForm()
+         .then(() => {
+            return updateProduct(product, product.id);
+         })
+         .then((product) => {
+            productManager.update(product.id, product);
+            closeForm();
+            reRender();
+            successToast.fire(`Update ${product.name} successfully 🎉`);
+         })
+         .catch((error) => {
+            errorToast.fire("Sever is busy 🏴 🏴 🏴");
+         })
+         .finally(() => {
+            button.disabled = false;
+         });
+   }
+});
 
 //=====================================================
 //    Xử lý chức năng tìm kiềm bằng search product
 //=====================================================
 
 function debounce(cb, delay = 300) {
-    let timeout
-    return (searchTerm) => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-            cb(searchTerm)
-        }, delay)
-    }
+   let timeout;
+   return (searchTerm) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+         cb(searchTerm);
+      }, delay);
+   };
 }
 
 const updateSearch = debounce((searchTerm) => {
-    // console.log(searchTerm)
-    getProducts(searchTerm)
-        .then(products => {
-            render(products)
-        })
-})
+   // console.log(searchTerm)
+   getProducts(searchTerm).then((products) => {
+      render(products);
+   });
+});
 
 dom("#searchInput").addEventListener("input", function (e) {
-    const searchTerm = this.value
-    updateSearch(searchTerm)
-})
-
-
-
-
-
+   const searchTerm = this.value;
+   updateSearch(searchTerm);
+});
 
 //=====================================================
-//          Lắng nghe sự kiện click vào 
+//          Lắng nghe sự kiện click vào
 //     các các item product để mở modal preview
 //          chứa toàn bộ thông tin sản phẩm
 //=====================================================
@@ -330,70 +347,76 @@ dom("#searchInput").addEventListener("input", function (e) {
 // })
 
 dom("#product__content").onclick = function (e) {
-    const element = e.target.closest(".product__item")
-    if (!element) return
-    const produceID = element.dataset.id
-    const product = productManager.findByID(produceID)
-    renderPreview(product)
-}
-
-
-
-
-
+   const element = e.target.closest(".product__item");
+   if (!element) return;
+   const produceID = element.dataset.id;
+   const product = productManager.findByID(produceID);
+   renderPreview(product);
+};
 
 //========================================
-//           Helper function 
+//           Helper function
 //========================================
 function dom(selector) {
-    return document.querySelector(selector)
+   return document.querySelector(selector);
 }
 
 function domAll(selector) {
-    return document.querySelectorAll(selector)
+   return document.querySelectorAll(selector);
 }
 
 function closeForm() {
-    dom("#formModal .modal__close").click()
+   dom("#formModal .modal__close").click();
 }
 
 function getFormValue() {
-    const productID = dom("#productID").value
-    const productCode = dom("#productCode").value
-    const productName = dom("#productName").value
-    const productPrice = +dom("#productPrice").value
-    const productBrand = dom("#productBrand").value
-    const productBackCamera = dom("#productBackCamera").value
-    const productFrontCamera = dom("#productFrontCamera").value
-    const productImg = dom("#productImg").value
-    const productScreen = dom("#productScreen").value
-    const productDesc = dom("#productDesc").value
+   const productID = dom("#productID").value;
+   const productCode = dom("#productCode").value;
+   const productName = dom("#productName").value;
+   const productPrice = +dom("#productPrice").value;
+   const productBrand = dom("#productBrand").value;
+   const productBackCamera = dom("#productBackCamera").value;
+   const productFrontCamera = dom("#productFrontCamera").value;
+   const productImg = dom("#productImg").value;
+   const productScreen = dom("#productScreen").value;
+   const productDesc = dom("#productDesc").value;
 
-    return new Product(productID, productCode, productName, productPrice, productScreen, productBackCamera, productFrontCamera, productImg, productDesc, productBrand)
+   return new Product(
+      productID,
+      productCode,
+      productName,
+      productPrice,
+      productScreen,
+      productBackCamera,
+      productFrontCamera,
+      productImg,
+      productDesc,
+      productBrand
+   );
 }
 
 function fillFormValue(product) {
-    dom("#productID").value = product.id
-    dom("#productCode").value = product.productCode
-    dom("#productName").value = product.name
-    dom("#productPrice").value = product.price
-    dom("#productBrand").value = product.brand
-    dom("#productScreen").value = product.screen
-    dom("#productBackCamera").value = product.backCamera
-    dom("#productFrontCamera").value = product.frontCamera
-    dom("#productImg").value = product.img
-    dom("#productDesc").value = product.desc
+   dom("#productID").value = product.id;
+   dom("#productCode").value = product.productCode;
+   dom("#productName").value = product.name;
+   dom("#productPrice").value = product.price;
+   dom("#productBrand").value = product.brand;
+   dom("#productScreen").value = product.screen;
+   dom("#productBackCamera").value = product.backCamera;
+   dom("#productFrontCamera").value = product.frontCamera;
+   dom("#productImg").value = product.img;
+   dom("#productDesc").value = product.desc;
 }
 
 function resetFormValue() {
-    dom("#productID").value = ''
-    dom("#productCode").value = ''
-    dom("#productName").value = ''
-    dom("#productPrice").value = ''
-    dom("#productBrand").value = ''
-    dom("#productScreen").value = ''
-    dom("#productBackCamera").value = ''
-    dom("#productFrontCamera").value = ''
-    dom("#productImg").value = ''
-    dom("#productDesc").value = ''
+   dom("#productID").value = "";
+   dom("#productCode").value = "";
+   dom("#productName").value = "";
+   dom("#productPrice").value = "";
+   dom("#productBrand").value = "";
+   dom("#productScreen").value = "";
+   dom("#productBackCamera").value = "";
+   dom("#productFrontCamera").value = "";
+   dom("#productImg").value = "";
+   dom("#productDesc").value = "";
 }
